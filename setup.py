e45c66427af04b4e084d1704489d7a7da30bf8d9
Noodle Deluxe <76448871+NoodleDX@users.noreply.github.com>
2023-08-21 20:46:54 -0600

    lmao i forgot to make this

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='crumbly',
+    version='0.1',
+    author='NoodleDX',
+    packages=find_packages(),
+    install_requires=[
+    ],
+)
